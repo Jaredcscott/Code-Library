5a89b60e64da912441681102a7303c8a901db7fd
Jared Work <jaredcscott93@gmail.com>
2021-06-28 16:05:26 -0600

    adding additional python scripts

---
+++ Python/Games/rockPaperScisors_Simple.py
@@ -0,0 +1,33 @@
+'''
+    Jared Scott
+    This script plays a single instance of rock paper scissors
+'''
+
+import random
+computersChoice = random.randint(0,2)
+playersChoice = eval(input("Please enter the number corresponding with your\
+ choice.\nscissor (0), rock (1), paper (2): "))
+
+if computersChoice == 0:
+    if playersChoice == 1:
+        print("The computer is scissor. You are rock. You win!")
+    elif playersChoice == 0:
+        print("The computer is scissor. You are scissor. It is a draw")
+    else:
+        print("The computer is scissor. You are paper. You lose.")
+
+if computersChoice == 1:
+    if playersChoice == 2:
+        print("The computer is rock. You are paper. You win!")
+    elif playersChoice == 1:
+        print("The computer is rock. You are rock. It is a draw")
+    else:
+        print("The computer is rock. You are scissor. You lose.")
+
+if computersChoice == 2:
+    if playersChoice == 0:
+        print("The computer is paper. You are scissor. You win!")
+    elif playersChoice == 2:
+        print("The computer is paper. You are paper. It is a draw")
+    else:
+        print("The computer is paper. You are rock. You lose.")
