77f2acf4f0d24c8e67e67af3b1cbcdc82a17aa4d
Jared Scott <34445278+Jaredcscott@users.noreply.github.com>
2021-07-16 00:05:25 -0600

    Adding in Sudoku Solver with notes

--- Python/Specific_Problems/Sudoku_Solver/sudokuSolver.py
+++ Python/Specific_Problems/Sudoku_Solver/sudokuSolver.py
@@ -1,38 +1,37 @@
 def main():
+    #----------------------CONSTANTS------------------------------------
     ATTEMPTS = 150
-    SOLUTIONS_FILEPATH = "C:\\Users\\jscott\\Desktop\\Code-Library\\Python\\Specific_Problems\\Sudoku_Solver\\solutions.dat"
-    PUZZLES_FILEPATH = "C:\\Users\\jscott\\Desktop\\Code-Library\\Python\\Specific_Problems\\Sudoku_Solver\\puzzles.txt"
-    
+    SOLUTIONS_FILEPATH = "C:\\Users\\jared\\Desktop\\solutions.dat"
+    PUZZLES_FILEPATH = "C:\\Users\\jared\\Desktop\\puzzles.txt"
+    #----------------------Loading previous solutions--------------------
     solutions = loadSols(SOLUTIONS_FILEPATH)
     print(len(solutions)," Solutions Loaded")
     if len(solutions) == 0:
         for i in range(1,51):
             solutions[i] = False
+    #----------------------Start of solution-----------------------------        
     puzzles = readPuzzles(PUZZLES_FILEPATH)
     for puzzle in puzzles:
-        #puzzle = puzzles[1]
         puzStr = puzzle.name.split(" ")[1].strip(' "\'\t\r\n')
         puzNum = int(puzStr)
         if str(solutions[puzNum]).strip(' "\'\t\r\n') == "False": 
             print("\nAttempting: " + puzzle.name)
-            puzzle.startRecursion()
-            puzzle.prettyPrint()
-            if puzzle.isSolved():
-                
-                solutions[puzNum] = puzzle.digits
-        
-    
+            puzzle.solveRecursive()
+            solutions[puzNum] = puzzle.digits 
+    #----------------------Reporting results-----------------------------
     solved = 0
     for puzzle in solutions:
         result = solutions[puzzle]
         if str(result).strip() != "False":
-            
             solved += 1 
-            
     print("\n" + str(solved) + "/50 Puzzles Solved!")
+    #----------------------Storing solutions-----------------------------
     file = open(SOLUTIONS_FILEPATH,"wt")
+    sum = 0
     for solution in solutions:
+        sum += int(solutions[solution])
         file.write(str(solution) + ":" + str(solutions[solution]).strip(' "\'\t\r\n') + "\n")
+    print("Sum: " + str(sum))
     file.close() 
     
 class Puzzle:
@@ -42,7 +41,137 @@ class Puzzle:
         self.cols = self.__extractCols()
         self.sqrs = self.__extractSqrs()
         self.digits = ""
-       
+                
+    def potentialValues(self,row,col):
+        #Assume all potentials
+        potentialValues = ['1','2','3','4','5','6','7','8','9']
+        #Ignore if value already present
+        if str(self.rows[row][col]) != "0":
+            return [self.rows[row][col]]
+        #Determining which 'square' the value exsists within
+        sqrIndex = self.getSqr(row,col)
+        #Populating values from associated groups 
+        checkRow = self.rows[row] 
+        checkCol = self.cols[col]
+        checkSqr = self.sqrs[sqrIndex]  
+        #Removing all chars which already present
+        for char in str(checkRow):
+            if char in potentialValues:
+                potentialValues.remove(char) 
+        for char in str(checkCol):
+            if char in potentialValues:
+                potentialValues.remove(char) 
+        for char in str(checkSqr):
+            if char in potentialValues:
+                potentialValues.remove(char)  
+        #Only potential values remain in list
+        return potentialValues
+        
+    def updateVal(self,row,col,val):
+        #Strings are immutable, creating list representation of row string
+        tempList = list(self.rows[row])
+        tempList[col] = val #Assigning value
+        #Replacing row String 
+        self.rows[row] = ''.join(tempList)
+        #Adjusting columns and square representations
+        self.cols = self.__extractCols() 
+        self.sqrs = self.__extractSqrs()  
+
+    def getEmptyCells(self):
+        cells = []
+        #Looping through grid, adding empty cell locations to the list of cells
+        for row in range(9):
+            for col in range(9):
+                curRow = self.rows[row]
+                if curRow[col] == '0':
+                    cellLoc = (row,col)
+                    cells.append(cellLoc)
+        return cells
+        
+    def solveRecursive(self):
+        #Checking if puzzle is solved 
+        if self.isSolved():
+            print("-----------------------------------------------------SOLUTION FOUND")
+            self.prettyPrint()
+            self.digits = self.rows[0][:3] #Storing numbers for final sum
+            return True
+        else:
+            #Generating a queue of empty cells 
+            queue = self.getEmptyCells()
+            #Pulling first item in queue
+            curRow = queue[0][0]
+            curCol = queue[0][1] 
+            #Looping through potential values at that cell location
+            for val in self.potentialValues(curRow,curCol):
+                #Trying value
+                self.updateVal(curRow,curCol,val)
+                if not self.solveRecursive():
+                    #Reseting value if solution not found 
+                    self.updateVal(curRow,curCol,'0') 
+            return False   
+    
+    def getSqr(self,row,col):
+        #Using range flags to determing which square the given location resides within
+        if row >= 0 and row < 3:
+            if col >= 0 and col < 3: 
+                sqrIndex = 0
+            elif col >= 3 and col < 6:
+                sqrIndex = 1
+            elif col >= 6 and col < 9:
+                sqrIndex = 2
+        elif row >= 3 and row < 6:
+            if col >= 0 and col < 3: 
+                sqrIndex = 3
+            elif col >= 3 and col < 6:
+                sqrIndex = 4
+            elif col >= 6 and col < 9:
+                sqrIndex = 5
+        elif row >= 6 and row < 9:
+            if col >= 0 and col < 3: 
+                sqrIndex = 6
+            elif col >= 3 and col < 6:
+                sqrIndex = 7
+            elif col >= 6 and col < 9:
+                sqrIndex = 8
+        return sqrIndex
+    
+    def solvePuzzle(self,times):
+        time = 0
+        emptySpaces = 81
+        while time < times:
+            for row in range(9):
+                for col in range(9):
+                    potVals = self.potentialValues(row,col)
+                    if len(potVals) == 1:
+                        self.updateVal(row,col,potVals[0])
+            if self.countEmpty() == 0:
+                print("Puzzle Solved!")
+                self.digits = self.rows[0][:3]
+                return
+            left = self.countEmpty()
+            if left < emptySpaces:
+                emptySpaces = left 
+                print("Empty Spaces: ",left)
+            time += 1         
+
+    def getCellVal(self,row,col):
+        return self.rows[row][col]
+                          
+    def countEmpty(self):
+        emptyCount = 0
+        for row in self.rows:
+            line = str(row)
+            for char in line:
+                if char == '0':
+                    emptyCount += 1
+        return emptyCount
+        
+    def isSolved(self):
+        if self.countEmpty() == 0:
+            return True 
+        else:
+            return False 
+            
     def __extractCols(self):
         cols = []
         for col in range(len(self.rows)):
@@ -53,6 +182,7 @@ class Puzzle:
         return cols 
         
     def __extractSqrs(self):
+        #Poorly written range flag square extraction
         sqrs = []
         left, center, right = [], [], []
         for row in range (0,3):
@@ -108,179 +238,6 @@ class Puzzle:
                 print('\n-----------')
             else:
                 print()
-                
-    def potentialValues(self,row,col):
-        potentialValues = ['1','2','3','4','5','6','7','8','9']
-        if str(self.rows[row][col]) != "0":
-            #print("Val Found!","Row: " + str(row), "Col: " + str(col),"Val: " + str(self.rows[row][col]))
-            return [self.rows[row][col]]
-            
-        sqrIndex = self.getSqr(row,col)
-        checkRow = self.rows[row] 
-        checkCol = self.cols[col]
-        checkSqr = self.sqrs[sqrIndex]  
-        
-        for char in str(checkRow):
-            if char in potentialValues:
-                potentialValues.remove(char) 
-        for char in str(checkCol):
-            if char in potentialValues:
-                potentialValues.remove(char) 
-        for char in str(checkSqr):
-            if char in potentialValues:
-                potentialValues.remove(char)     
-        #print("row: " + str(row), "Col: " + str(col),checkRow,checkCol,checkSqr,potentialValues)
-                
-        return potentialValues
-    
-    def getSqr(self,row,col):
-        if row >= 0 and row < 3:
-            if col >= 0 and col < 3: 
-                sqrIndex = 0
-            elif col >= 3 and col < 6:
-                sqrIndex = 1
-            elif col >= 6 and col < 9:
-                sqrIndex = 2
-        elif row >= 3 and row < 6:
-            if col >= 0 and col < 3: 
-                sqrIndex = 3
-            elif col >= 3 and col < 6:
-                sqrIndex = 4
-            elif col >= 6 and col < 9:
-                sqrIndex = 5
-        elif row >= 6 and row < 9:
-            if col >= 0 and col < 3: 
-                sqrIndex = 6
-            elif col >= 3 and col < 6:
-                sqrIndex = 7
-            elif col >= 6 and col < 9:
-                sqrIndex = 8
-        return sqrIndex
-    
-    def solvePuzzle(self,times):
-        time = 0
-        emptySpaces = 81
-        while time < times:
-            for row in range(9):
-                for col in range(9):
-                    potVals = self.potentialValues(row,col)
-                    if len(potVals) == 1:
-                        self.updateVal(row,col,potVals[0])
-            #self.fillSingles()   
-            if self.countEmpty() == 0:
-                print("Puzzle Solved!")
-                self.digits = self.rows[0][:3]
-                return
-            left = self.countEmpty()
-            if left < emptySpaces:
-                emptySpaces = left 
-                print("Empty Spaces: ",left)
-            time += 1
-            #if time % 50 == 0:
-            #    print("Solving ...")
-            
-            #if time == times:
-            #    print("No Solution Found After " + str(times) + " Attempts")   
-    
-    def startRecursion(self):
-        puzzleState = {}
-        for row in range(9):
-            for col in range(9):
-                puzzleState[(row,col)] = [self.rows[row][col]]
-        self.solveRecursive(puzzleState)
-    
-    def solveRecursive(self, puzzleState):
-        if self.isSolved():
-            print("-----------------------------------------------------SOLUTION FOUND")
-            puzzle.prettyPrint()
-            return True
-        else:    
-            queue = self.getEmptyCells()
-            for cell in queue:
-                puzzleState[cell] = self.potentialValues(cell[0],cell[1])
-            for cell in puzzleState:
-                nums = puzzleState[cell]
-                if len(nums) > 1:
-                    for num in nums:
-                        temp = self.getCellVal(cell[0],cell[1])
-                        self.updateVal(cell[0],cell[1],num)
-                        puzzleState[cell] = str(num)
-                        if self.solveRecursive(puzzleState):
-                            return True
-                        else:
-                            return False
-                        self.updateVal(cell[0],cell[1],temp)
-                elif len(nums) == 0:
-                    print(cell)
-                else:
-                    self.updateVal(cell[0],cell[1],nums[0])
-            return False         
-            
-    def getEmptyCells(self):
-        cells = []
-        for row in range(9):
-            for col in range(9):
-                curRow = self.rows[row]
-                if curRow[col] == '0':
-                    cellLoc = (row,col)
-                    cells.append(cellLoc)
-        return cells
-
-    def getCellVal(self,row,col):
-        return self.rows[row][col]
-            
-    def tempStorage(self):        
-        self.prettyPrint()
-        lowestNeed = 9
-        lowestInx = 0
-        inx = 0
-        for sqr in self.sqrs:
-            need = 9 - len(self.contains(sqr))
-            if need < lowestNeed:
-                lowestNeed = need 
-                lowestInx = inx
-            inx += 1
-        self.needs(self.sqrs[lowestInx])
-        self.reduceSqr(2)
-        #print(self.potentialValues(0,8))
-        #print("Need: " + str(lowestNeed) + " Sqr: " + str(lowestInx))
-        self.contains(self.sqrs[0])            
-    
-    def contains(self,vals):
-        nums = []
-        for val in vals:
-            if val not in nums and val != '0':
-                nums.append(val)
-        return nums
-    
-    def needs(self,vals):
-        nums = ['1','2','3','4','5','6','7','8','9']
-        for val in vals:
-            if val in nums:
-                nums.remove(val)
-        return nums
-    
-    def updateVal(self,row,col,val):
-        tempList = list(self.rows[row])
-        tempList[col] = val
-        self.rows[row] = ''.join(tempList)
-        self.cols = self.__extractCols() 
-        self.sqrs = self.__extractSqrs()
-        
-    def countEmpty(self):
-        emptyCount = 0
-        for row in self.rows:
-            line = str(row)
-            for char in line:
-                if char == '0':
-                    emptyCount += 1
-        return emptyCount
-        
-    def isSolved(self):
-        if self.countEmpty() == 0:
-            return True 
-        else:
-            return False 
             
     def __str__(self):
         return "Name: " + self.name + "\nRows: " + str(self.rows) + "\nCols: " + str(self.cols) + "\nSqrs: " + str(self.sqrs) + "\n\n"
@@ -293,6 +250,7 @@ def loadSols(filepath):
         for solution in data:
             curSol = solution.split(":")
             solutions[int(curSol[0])] = curSol[1]
+    file.close()
     return solutions   
 
 def readPuzzles(filepath):    
