4b089cdb00d0927372e2eb2abf1c8f40c385cf16
Jared Work <jaredcscott93@gmail.com>
2021-08-16 15:58:43 -0600

    addin gin a Traffic Jam solver for Java

---
+++ Java/Traffic_Jam_Solver/Main.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.text.*;
+import java.util.*;
+
+/**
+ * The main begins by reading in
+ * all of the puzzles described in a file named jams.txt.
+ * It then proceeds to run a brute force solution. In each case, it prints out the solution
+ * path that was computed.
+ */
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        // read all the puzzles in file.  Only the first few are solvable without additional strategies
+        Puzzle[] puzzles = Puzzle.readPuzzlesFromFile("jamsAll.txt");
+        int num_puzzles = puzzles.length;
+
+        boolean doPrint = true;
+        
+        //Solve each of the first six puzzles.  The others will likely take too long
+        for (int i = 0; i < num_puzzles; i++) {
+            puzzles[i].solve(doPrint);       
+        }
+    }
+}
