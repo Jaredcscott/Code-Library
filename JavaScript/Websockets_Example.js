3ade3f4ac6504553705d13a3e8c686d08200d8d7
Jared Work <jaredcscott93@gmail.com>
2021-10-11 11:59:09 -0600

    Added in a websockets example

---
+++ JavaScript/Websockets_Example.js
@@ -0,0 +1,6 @@
+function sendCommand(command) {
+	var HOST = '192.168.176.54';
+	var PORT = '10321';
+	var ws = new WebSocket("ws://" + HOST + ":" + PORT + "/");
+	ws.onopen = () => ws.send("RUN TEST:I2-R5-T7");
+}
