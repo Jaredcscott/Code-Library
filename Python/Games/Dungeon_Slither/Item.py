aa2f8f4d7b147e4c276d4209a0b6d7fbf050791c
Jared Scott <34445278+Jaredcscott@users.noreply.github.com>
2021-10-20 16:24:19 -0600

    Create Item.py

---
+++ Python/Games/Dungeon_Slither/Item.py
@@ -0,0 +1,14 @@
+class Item:
+    def __init__(self,name,type,value):
+        self.name = name 
+        self.type = type
+        self.value = value  
+        
+    def setValue(self,value):
+        self.value = value 
+        
+    def getValue(self):
+        return self.value
+      
+    def __str__(self):
+        return "Item: " + str(self.name) + " of Type: " + str(self.type) + " Value: " + str(self.value)
