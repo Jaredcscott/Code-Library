1264a580e8cd06c7a63731f8cf6aed932b60abca
Jared Scott <34445278+Jaredcscott@users.noreply.github.com>
2021-10-20 16:20:11 -0600

    Adding default health, attack and defensive values for all actors, created methods common between all actors.

--- Python/Games/Dungeon_Slither/Actor.py
+++ Python/Games/Dungeon_Slither/Actor.py
@@ -2,7 +2,19 @@ class Actor:
     def __init__(self,id,type=None):
         self.id = id
         self.type = type
-
+        self.health = 10
+        self.atk = 1 
+        self.def = 1
+    
+    def setHealth(self,value):
+        self.health = value
+    
+    def setAtk(self,value):
+        self.atk = value
+        
+    def setDef(self,value):
+        self.def = value
+        
     def __str__(self):
         return "Actor ID: " + str(self.id) + " " + str(self.type) 
 
