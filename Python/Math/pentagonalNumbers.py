7acb84dc2424fa23c0b5e95558f4d65d23ecad45
Jared Scott <34445278+Jaredcscott@users.noreply.github.com>
2021-10-25 10:51:27 -0600

    Update pentagonalNumbers.py

--- Python/Math/pentagonalNumbers.py
+++ Python/Math/pentagonalNumbers.py
@@ -1,22 +1,33 @@
+'''
+    @Author Jared Scott 
+    This script generates then compared pentagonal numbers 
+'''
+
 import math
 
-pentagonalNums = [] 
-masterNums = []
+def main():
+    pentagonalNums = [] 
+    masterNums = []
+    limit = 3000
+    
+    generatePentagonalNumbers(limit,pentagonalNums)
+    for numA in pentagonalNums:
+        for numB in pentagonalNums:
+            if compareNums(numA,numB):
+                print(numA,numB,abs(numA-numB))
 
-def generatePentagonalNumbers(limit):
+def generatePentagonalNumbers(limit,numList):
+    #This function will fill the given numList with limit number of pentagonal numbers
     num = 1
     while num < limit:
         curNum = (num * ((3 * num) - 1)) / 2 
-        pentagonalNums.append(int(curNum))
+        numList.append(int(curNum))
         num += 1
         
 def compareNums(numA,numB):
     sum1 = numA + numB
     sub = abs(numA - numB) 
-    return (sum1 in pentagonalNums and sub in pentagonalNums)
+    return ((sum1 in pentagonalNums) and (sub in pentagonalNums))
     
-generatePentagonalNumbers(3000)
-for numA in pentagonalNums:
-    for numB in pentagonalNums:
-        if compareNums(numA,numB):
-            print(numA,numB,abs(numA-numB))
+if __name__ == '__main__':
+    main()
