14ce2597e6e790e828f7ad2e410e2f1210a701dd
Jared Scott <34445278+Jaredcscott@users.noreply.github.com>
2021-10-26 14:55:17 -0600

    Update polygonProperties.py

--- Python/Math/polygonProperties.py
+++ Python/Math/polygonProperties.py
@@ -1,15 +1,18 @@
 '''
-    Jared Scott
+    @Author Jared Scott
     This program will take inputs from the user, namely the sides and length of a given
     polygon. the program will perform some calculations and output the area of the polygon
     to the user.
 '''
-
 import math
-from math import tan
-numOfSides = eval(input("Enter the number of sides: "))
-sideLength = eval(input("Enter the side length: "))
 
-area = (numOfSides * math.pow(sideLength,2))/(4 * tan(math.pi/numOfSides))
+def main():
+    numOfSides = eval(input("Enter the number of sides: "))
+    sideLength = eval(input("Enter the side length: "))
 
-print("The area of the polygon is: {0}".format(str(area)))
+    area = (numOfSides * math.pow(sideLength,2))/(4 * math.tan(math.pi/numOfSides))
+
+    print("The area of the polygon is: {0}".format(str(area)))
+
+if __name__ == '__main__':
+        main()
