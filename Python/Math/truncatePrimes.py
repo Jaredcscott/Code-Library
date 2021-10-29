1df7b3354c5e96c70c18dbf3f9455eed5f8c5bf1
Jared Scott <34445278+Jaredcscott@users.noreply.github.com>
2021-10-29 15:01:02 -0600

    Update truncatePrimes.py

--- Python/Math/truncatePrimes.py
+++ Python/Math/truncatePrimes.py
@@ -1,5 +1,5 @@
 '''
-    @author Jared Scott
+    @Author Jared Scott
     Prob 37 of Project Euler 
 '''
 import math 
@@ -16,41 +16,47 @@ def isPrime(numI):
         return True
     else:
         return False
-        
-primes = []
-for i in range(750000):
-    if isPrime(i) and i > 10:
-        primes.append(i)
-        
+    
 def truncateLeft(num):
+    #This function removes one character from the left of the given string 
     numStr = str(num) 
     num = numStr[1:]
     return num
+
 def truncateRight(num):
+    #this function removes one character from the right of the given string 
     numStr = str(num) 
     num = numStr[:-1]
     return num
+    
+def main():        
+    primes = []
+    for i in range(750000):
+        if isPrime(i) and i > 10:
+            primes.append(i)
 
-sumPrimes = []
-sumP = 0
-for prime in primes:
-    addToSum = True 
-    curNum = prime
-    while len(str(curNum)) > 1:
-        newNum = truncateLeft(curNum)
-        if not isPrime(newNum):
-           addToSum = False 
-        curNum = newNum 
-    curNum = prime
-    while len(str(curNum)) > 1:
-        newNum = truncateRight(curNum)
-        if not isPrime(newNum):
-           addToSum = False    
-        curNum = newNum 
-        
-    if addToSum:
-        sumPrimes.append(prime)
-        sumP += prime
-        
-print(sumP," : ",sumPrimes) 
+    sumPrimes = []
+    sumP = 0
+    for prime in primes:
+        addToSum = True 
+        curNum = prime
+        while len(str(curNum)) > 1:
+            newNum = truncateLeft(curNum)
+            if not isPrime(newNum):
+               addToSum = False 
+            curNum = newNum 
+        curNum = prime
+        while len(str(curNum)) > 1:
+            newNum = truncateRight(curNum)
+            if not isPrime(newNum):
+               addToSum = False    
+            curNum = newNum 
 
+        if addToSum:
+            sumPrimes.append(prime)
+            sumP += prime
+
+    print(sumP," : ",sumPrimes) 
+
+if __name__ == '__main__':
+    main()
