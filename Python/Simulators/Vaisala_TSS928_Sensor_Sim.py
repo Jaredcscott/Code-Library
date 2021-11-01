90a3a79988b627a47b3b2d8429ddaaa166912353
Jared Scott <34445278+Jaredcscott@users.noreply.github.com>
2021-11-01 13:38:36 -0600

    Update Vaisala_TSS928_Sensor_Sim.py

--- Python/Simulators/Vaisala_TSS928_Sensor_Sim.py
+++ Python/Simulators/Vaisala_TSS928_Sensor_Sim.py
@@ -1,11 +1,19 @@
+'''
+   @Author Jared Scott
+   This script will open a socket connection at the specified port number hosted from localhost (127.0.0.1)
+   This socket when conntacted will generate a random data value and return this data over the socket.
+'''
+
 import socket
 import random
 
-port = 10170  #Port where the sensor data will be 'Served'
-sckt = socket.socket()
-sckt.bind(('localhost', port))  # Creates the port binding for 127.0.0.1:10150
-sckt.listen()  #Socket object will now listen for activity on the specified port
-print('Server listening....')
+def main():
+    port = 10170  #Port where the sensor data will be 'Served'
+    sckt = socket.socket()
+    sckt.bind(('localhost', port))  # Creates the port binding for 127.0.0.1:10150
+    sckt.listen()  #Socket object will now listen for activity on the specified port
+    print('Server listening....')
+    simulateSensor(sckt)
 
 def fillDicts():
     data = {"nearN":0,"nearNE":0,"nearE":0,"nearSE":0,"nearS":0,"nearSW":0,"nearW":0,"nearNW":0,
@@ -15,9 +23,9 @@ def fillDicts():
         data[item] = str(random.randint(10000,29999))
     return data
 
-def genVSS928Data():
+def genVSS928Data(dataDict):
     #See ICD for Vaisala TSS924
-    data = fillDicts()
+    data = dataDict
     dataStringNear = "NEAR: N "+data["nearN"]+" NE "+data["nearNE"]+" E "+data["nearE"] +" SE "+data["nearSE"]+\
                      " S "+data["nearS"]+" SW "+data["nearSW"]+" W "+data["nearW"]+" NW "+data["nearNW"]+"\x0D\x0A"
     dataStringDist = "DIST: N "+data["distN"]+" NE "+data["distNE"]+" E "+data["distE"] +" SE "+data["distSE"]+" S "+\
@@ -26,17 +34,19 @@ def genVSS928Data():
 
     return dataStringNear + dataStringDist + dataStringMisc
 
-def simulateSensor():
-    dataString = genVSS928Data()  # Generating simulated data
+def simulateSensor(sckt):
+    dict = fillDicts()
+    dataString = genVSS928Data(dict)  # Generating simulated data
     conn, address = sckt.accept()  # Establish connection with client.
     print('Incoming connection from address: ', str(address[0]) + ":" + str(address[1]))
     while True:
         #Simulator waits for a 'poll' then sends a data string
         data = conn.recv(1024)
         print('Incoming Poll Message:', data.decode())
-        dataString = genVSS928Data() #Generating simulated data
+        dataString = genVSS928Data(dict) #Generating simulated data
         byt = dataString.encode()
         conn.send(byt)
     conn.close()
 
-simulateSensor()
+if __name__ == '__main__':
+    main()
