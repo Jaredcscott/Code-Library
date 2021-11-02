174765a647b83e9fc00aaf4e7c6412196bc44496
Jared Scott <34445278+Jaredcscott@users.noreply.github.com>
2021-11-02 12:03:56 -0600

    Update GEDruck_Sensor_Sim.py

--- Python/Simulators/GEDruck_Sensor_Sim.py
+++ Python/Simulators/GEDruck_Sensor_Sim.py
@@ -9,30 +9,35 @@ import random
 import time
 
 def main():
-    port = 10102  #Port where the sensor data will be 'Served'
-    sckt = socket.socket()
-    sckt.bind(('localhost', port))  # Creates the port binding for 127.0.0.1:10150
-    sckt.listen()  #Socket object will now listen for activity on the specified port
-    print('Serving GEDRuck Data on at 127.0.0.1:10102\nListening....')
-    simulateSensor(sckt)
+   port = 10102  #Port where the sensor data will be 'Served'
+   sckt = socket.socket()
+   sckt.bind(('localhost', port))  # Creates the port binding for 127.0.0.1:10102
+   sckt.listen()  #Socket object will now listen for activity on the specified port
+   print('Serving GEDRuck Data on at 127.0.0.1:' + str(port) + '\nListening....')
+   while True:
+      conn = None
+      try:
+         conn, address = sckt.accept()  # Establish connection with client
+         print('Incoming connection from address: ', str(address[0]) + ":" + str(address[1]))
+         simulateSensor(conn,address)
+      except KeyboardInterrupt:
+         print("Shutting down sensor sim. . .")
+         if conn:
+            conn.close()
+         break      
 
 def genGEDruckData():
-    randomValue = random.randint(850,1050)
-    dataString = "1: " + str(randomValue) + "\r"
-    return dataString
+   randomValue = random.randint(850,1050)
+   dataString = "1: " + str(randomValue) + "\r"
+   return dataString
 
 def simulateSensor(sckt):
-    conn, address = sckt.accept()  # Establish connection with client.
-    print('Incoming connection from address: ', str(address[0]) + ":" + str(address[1]))
-    while True:
-        #Simulator waits for a 'poll' then sends a data string
-        #data = conn.recv(1024)
-        #print('Incoming Poll Message:', data.decode())
-        dataString = genGEDruckData() #Generating simulated data
-        byt = dataString.encode()
-        conn.send(byt)
-        time.sleep(10)
-    conn.close()
+   #Simulator waits for a 'poll' then sends a data string
+   #data = conn.recv(1024)
+   #print('Incoming Poll Message:', data.decode())
+   dataString = genGEDruckData() #Generating simulated data
+   byt = dataString.encode()
+   conn.send(byt)
 
 if __name__ == '__main__':
     main()
