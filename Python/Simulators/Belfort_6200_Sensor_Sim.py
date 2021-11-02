ce24aa5775b2487be2830a67fd485afa86b47676
Jared Scott <34445278+Jaredcscott@users.noreply.github.com>
2021-11-02 12:21:30 -0600

    Update Belfort_6200_Sensor_Sim.py

--- Python/Simulators/Belfort_6200_Sensor_Sim.py
+++ Python/Simulators/Belfort_6200_Sensor_Sim.py
@@ -12,25 +12,41 @@ def main():
     sckt = socket.socket()
     sckt.bind(('localhost', port))  # Creates the port binding for 127.0.0.1:10150
     sckt.listen()  #Socket object will now listen for activity on the specified port
-    print('Server listening....')
-    simulateSensor(sckt)
+    sckt.settimeout(3)
+    print('Serving Belfort 6200 Data at 127.0.0.1:' + str(port) + '\nListening....')
+    while True:
+      conn = None
+      try:
+         #Outer try is watching for a keyboard interrupt coming from the inner try when it throws a timeout exception 
+         try:
+            '''
+               KeyboardInterrupt does not reach main call stack until process is complete. 
+               The timeout will complete the process and exit with a timeout exception.
+               If the user has entered a keyboard interrupt, the timeout will allow that exception to be caught
+            '''
+             conn, address = sckt.accept()  # Establish connection with client.
+             print('Incoming connection from address: ', str(address[0]) + ":" + str(address[1]))
+             simulateSensor(conn)
+        except socket.timeout:
+            print("Listening....")
+            continue
+      except KeyboardInterrupt:
+         print("Shutting down sensor sim. . .")
+         if conn:
+            conn.close()
+         break
 
 def genBelfortData():
     randomValue = random.uniform(12,14)
     dataString = "\x01\x02\x0DVP" + str(randomValue)[:5] + "DE4" + "\r\n"
     return dataString
 
-def simulateSensor(sckt):
-    conn, address = sckt.accept()  # Establish connection with client.
-    print('Incoming connection from address: ', str(address[0]) + ":" + str(address[1]))
-    while True:
-        #Simulator waits for a 'poll' then sends a data string
-        data = conn.recv(1024)
-        print('Incoming Poll Message:', data.decode())
-        dataString = genBelfortData() #Generating simulated data
-        byt = dataString.encode()
-        conn.send(byt)
-    conn.close()
+def simulateSensor(conn):
+   data = conn.recv(1024)
+   print('Incoming Poll Message:', data.decode())
+   dataString = genBelfortData() #Generating simulated data
+   byt = dataString.encode()
+   conn.send(byt)
     
 if __name__ == '__main__':
     main()
