2eb08a67c2e0b587368653a96115766ced3f4bbd
Jared Scott <34445278+Jaredcscott@users.noreply.github.com>
2021-12-08 10:12:39 -0700

    Update MainForm.java

--- Java/Clock/Clock/src/MainForm.java
+++ Java/Clock/Clock/src/MainForm.java
@@ -4,16 +4,9 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import javax.swing.JOptionPane;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
- * @author jareds
+ * @author Jared Scott ☯
  */
 public class MainForm extends javax.swing.JFrame {
 
