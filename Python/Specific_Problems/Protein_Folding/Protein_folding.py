9ed41effcf94572803a32e623a6392d59c51de0d
Jared Work <jaredcscott93@gmail.com>
2022-01-20 17:00:00 -0700

    Adding in protein folding problem as well as the reworked implementation for the 735 euler problem

--- Python/Specific_Problems/Protein_Folding/Protein_folding.py
+++ Python/Specific_Problems/Protein_Folding/Protein_folding.py
@@ -1,14 +1,26 @@
 class Protein:
+    #Class structure to represent a protein and compartmentalize analysis
     def __init__(self,string):
         self.string = string
-
+        self.grid = generate_grid(len(self.string))
+        
+    def print(self):
+        print("Folded protein generated from: " + self.string)
+        for row in self.grid:
+            print(row)
+        
+def generate_grid(grid_size):
+    '''
+    A function which will generate a square grid of the given size 
+    All values are initialized to an empty string
+    '''
+    grid = [["" for j in range(grid_size)] for i in range(grid_size)]
+    return(grid)
 
 def main():
     example = "HHPPHHHPHHPH"
     pro = Protein(example) 
-    print(pro.string)
-
-
-
+    pro.print()
+    
 if __name__ == "__main__":
     main()
